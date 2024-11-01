c4d0abfffc02c7f1ba68a2960d28ee910a13bfab
Kyle Snow <kylecsnow@gmail.com>
2024-11-01 09:54:29 -0400

    add placeholder for correlation heatmaps page

---
+++ frontend/app/correlation-heatmaps/page.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
+const CorrelationHeatmapsPage = () => {
+  
+  return (
+      <div>
+        <h1>Under construction...</h1>
+      </div>
+  )
+}
+
+export default CorrelationHeatmapsPage
