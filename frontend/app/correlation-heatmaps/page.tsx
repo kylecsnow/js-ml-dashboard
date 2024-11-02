8bc30650780234619b2ea55ce75f206839fe4f60
Kyle Snow <kylecsnow@gmail.com>
2024-11-02 15:47:26 -0400

    add button and link back & forth from homepage to other pages

--- frontend/app/correlation-heatmaps/page.tsx
+++ frontend/app/correlation-heatmaps/page.tsx
@@ -1,11 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import Image from "next/image";
+import Link from 'next/link'  // Add this import at the top
 
 const CorrelationHeatmapsPage = () => {
   
   return (
+    <div>
+      <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <Link
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          href="./"
+          // target="_blank"  exclude this to stop the link from always opening in a new tab
+          >
+          <Image
+            className="dark:invert"
+            src="/snowflake.svg"
+            alt="My snowflake logomark"
+            width={20}
+            height={20}
+            />
+          Home
+        </Link>
+      </div>
       <div>
         <h1>Under construction...</h1>
       </div>
+    </div>
   )
 }
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from 'next/link'  // Add this import at the top
 
 export default function Home() {
   return (
@@ -47,6 +48,22 @@ export default function Home() {
           >
             Read our docs
           </a>
+        </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <Link
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="./correlation-heatmaps"
+            // target="_blank"  exclude this to stop the link from always opening in a new tab
+            >
+            <Image
+              className="dark:invert"
+              src="/snowflake.svg"
+              alt="My snowflake logomark"
+              width={20}
+              height={20}
+            />
+            Correlation Heatmaps
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
