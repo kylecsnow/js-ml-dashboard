f8b99fecb17db49165b1534c945dbcb56e4ff86a
Kyle Snow <kylecsnow@gmail.com>
2024-11-10 11:12:50 -0500

    add left-padding to sidebar highlighting

--- frontend/app/components/Sidebar.tsx
+++ frontend/app/components/Sidebar.tsx
@@ -12,21 +12,21 @@ export default function Sidebar() {
       <nav className="flex flex-col gap-[6px]">
         <Link 
           href="/"
-          className={`flex items-center gap-2 p-1 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 text-[14.5px]
+          className={`flex items-center gap-2 p-1 pl-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 text-[14.5px]
             ${pathName === '/' ? 'bg-black text-white' : ''}`}
         >
           Home
         </Link>
         <Link 
           href="/overview"
-          className={`flex items-center gap-2 p-1 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 text-[14.5px]
+          className={`flex items-center gap-2 p-1 pl-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 text-[14.5px]
             ${pathName === '/overview' ? 'bg-black text-white' : ''}`}
         >
           Overview
         </Link>
         <Link 
           href="/correlation-heatmaps"
-          className={`flex items-center gap-2 p-1 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 text-[14.5px]
+          className={`flex items-center gap-2 p-1 pl-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 text-[14.5px]
             ${pathName === '/correlation-heatmaps' ? 'bg-black text-white' : ''}`}
         >
           Correlation Heatmaps
