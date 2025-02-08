13336afedfea9681f4165f7c950a9c8f26be5af7
Kyle Snow <kylecsnow@gmail.com>
2025-02-08 18:18:26 -0500

    add SHAP waterfall plot page, correctly this time, still WIP

---
+++ frontend/app/shap-waterfall-plots/page.tsx
@@ -0,0 +1,153 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import Image from "next/image";
+import Link from 'next/link';
+import Select from 'react-select';
+import Sidebar from '../components/Sidebar';
+import { useState, useEffect } from 'react';
+import { useModel } from '../contexts/ModelContext';
+
+const Plot = dynamic(() => import('react-plotly.js'), { ssr: false });
+
+// TODO: don't re-make this interface in every page; pull it out into some other place and then import it in each of your pages.
+interface PlotDataType {
+    data: any[];
+    layout: any;
+}
+
+const ShapSummaryPlotsPage = () => {
+  const { selectedModel } = useModel();
+  const [plotData, setPlotData] = useState<PlotDataType | null>(null);
+  const [outputVariableOptions, setOutputVariableOptions] = useState<{ value: string; label: string }[]>([]);
+  const [selectedOutputVariable, setSelectedOutputVariable] = useState<string[]>();
+
+
+  useEffect(() => {
+    const fetchOutputVariableOptions = async () => {
+      if (selectedModel) {
+        try {
+          // const response = await fetch(`http://localhost:8000/api/output-variable-options/${selectedModel}`);
+          const response = await fetch(`./api/output-variable-options/${selectedModel}`);
+          const data = await response.json();
+          const options = data.output_variable_options.map((option: string) => ({ value: option, label: option }));
+          setOutputVariableOptions(options);
+
+          // Set the first option as selected by default
+          if (options.length >= 1) {
+            setSelectedOutputVariable(options[0].value);
+          }
+        } catch (error) {
+          console.error('Error fetching variable options:', error);
+        }
+      }
+    };
+
+    fetchOutputVariableOptions();
+  }, [selectedModel]);
+
+
+  
+  useEffect(() => {
+    async function fetchShapWaterfallPlotData() {
+      try {
+        const response = await fetch(
+          `./api/shap-waterfall-plots/${selectedModel}`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ selected_output: selectedOutputVariable }), // Send as JSON
+          }
+        );
+        const data = await response.json();
+        setPlotData(data.plot_data);
+      } catch (error) {
+        console.error('Error fetching scatter plot data:', error);
+      }
+    };
+
+    fetchShapWaterfallPlotData();
+  }, [selectedModel, selectedOutputVariable]);
+
+
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      
+      <div className="flex-1 flex flex-col items-center p-8 gap-8">
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <Link
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="./"
+          >
+            <Image
+              className="dark:invert"
+              src="/snowflake.svg"
+              alt="My snowflake logomark"
+              width={20}
+              height={20}
+            />
+            Home
+          </Link>
+
+        </div>
+        <div>
+          <h2>
+            {selectedModel
+              ? `Selected model: ${selectedModel}`
+              : 'No model selected'
+            }
+          </h2>
+        </div>
+        <div className="relative">
+        <label>{"Selected output variable:"}</label>
+        <Select
+            options={outputVariableOptions}
+            onChange={(selected: { value: string; label: string } | null) => {
+              if (selected) {
+                setSelectedOutputVariable([selected.value]);
+              }
+            }}
+            value={outputVariableOptions.filter(option => selectedOutputVariable?.includes(option.value))} // Set selected values
+            name="selected-variables"
+            classNamePrefix="select"
+          />
+        <label>{"Selected a sample from the dataset::"}</label>
+        <Select
+            options={outputVariableOptions}
+            onChange={(selected: { value: string; label: string } | null) => {
+              if (selected) {
+                setSelectedOutputVariable([selected.value]);
+              }
+            }}
+            value={outputVariableOptions.filter(option => selectedOutputVariable?.includes(option.value))} // Set selected values
+            name="selected-variables"
+            classNamePrefix="select"
+          />        
+        </div>
+        <div>
+          <h1>Under construction...</h1>
+          <h3>TODOs:</h3>
+          <ol className="list-decimal ml-6">
+            <li>Get the page working!!! (need to add backend methods to main.py)</li>
+            <li>get loading animation working</li>
+          </ol>
+        </div>
+        <div className="w-full max-w-8xl mx-auto">
+          {plotData && (
+            <Plot
+              data={plotData.data}
+              layout={plotData.layout}
+              config={{ responsive: true }}
+              style={{ width: '100%', height: '750px' }}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default ShapSummaryPlotsPage;
